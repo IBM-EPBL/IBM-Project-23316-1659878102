45b91b5740b043fa38403cccb4b650248150ac89
Nideesh-K-G <102233372+Nideesh-K-G@users.noreply.github.com>
2022-10-17 14:21:22 +0530

    Update Assignment_2.py

--- Assignments/2.Nideesh K G/Assignment 2/Assignment_2.py
+++ Assignments/2.Nideesh K G/Assignment 2/Assignment_2.py
@@ -4,10 +4,11 @@ temp=r.randrange(0,180)
 humidity=r.randrange(0,100)
 print(temp)
 print(humidity)
-if temp>80:
-    if humidity >80:
+if temp>80 and humidity >80:
         print("Alarm beep")
-elif temp==80:
-    print("High temp")
+elif temp>80 and humidity<80:
+    print("High temp and low humidity")
+elif temp <80 and humidity>80:
+    print("Low temp and high humidity")
 else:
     print("All good")
